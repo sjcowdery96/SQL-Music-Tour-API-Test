6f0b39cf272812b9dbb864748d3ba138098aa2a7
sjcowdery96 <sjc.cowdery@gmail.com>
2024-01-30 12:21:44 -0600

    added server comments

---
+++ README.md
@@ -0,0 +1,11 @@
+##Overview
+This assignment is a solution copy populated with comments as an agreed substitute to a final submission. 
+
+The comments with three "///" marks are where I have added clarification and documentation to the assignment.
+
+All other comments have been retained from the original solution.
+
+I certify I did not use an LLM to quickly sort this code but only used google searches to clarify areas and concepts in small batches.
+
+Signed,
+Sam Cowdery
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 // DEPENDENCIES
+/// brings in the necessary node modules and packages required to run the app
 const express = require('express')
 const app = express()
+///sequelize is used to mediate between the SQL database and javascript
+///it also will generate migration files for each model so data translates properly
 const { Sequelize } = require('sequelize')
-const bands = require('./controllers/bands_controller')
+
+///the controlerfile below is unnecessary as it is defined and used later
+/// --> const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
+/// initializes our app to run express and configure our environmental varables
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // ROOT
+///this is the base of our app that will fire when the user first lands on the homepage
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
@@ -17,6 +24,8 @@ app.get('/', (req, res) => {
 })
 
 // CONTROLLERS  
+
+///these controlers are used to control calls and responses to each of the corresponding data tables
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
 
@@ -27,6 +36,8 @@ const stagesController = require('./controllers/stages_controller')
 app.use('/stages', stagesController)
 
 // LISTEN
+
+///finally we have the server listen on our port
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
